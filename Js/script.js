0f897a752f3860922f80e21ed89fe658b6b87323
ZhekaGrushevskiy <grushevskiy.yevgeniy@mail.ru>
2021-12-14 16:58:06 +0300

    new version

--- Js/script.js
+++ Js/script.js
@@ -2,13 +2,13 @@
 
 let fname,lname,sname,age,gender,pencia; 
 
-while (fname == "" || fname == null) {
+while (fname == "" || fname == null || fname.length>15) {
     fname = prompt('Ваше Имя?', "Евгений");
 }
-while (lname == "" || lname == null) {
+while (lname == "" || lname == null || lname.length>15) {
     lname = prompt('Ваша Фамилия?', "Грушевский");
 }
-while (sname == "" || sname == null) {
+while (sname == "" || sname == null || sname.length>15) {
     sname = prompt('Ваше Отчество ?', "Сергеевич")
 }
 while (age == "" || isNaN(age) || age == null ||
@@ -29,6 +29,7 @@ if ((gender && age > 65) || (!gender && age > 60)) {
 gender = gender ? "Мужской" : "Женский";
 
 if (fname != "" && lname != "" && sname != "" &&
+    fname.length<15 && lname.length<15 && sname.length<15 &&
     fname != null && lname != null && sname != null &&
     age != "" && +age != NaN) {
     alert(`
